c7c2504ed6c07dfc9dfc9db5ad708bdd07dcaeb8
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-08 21:46:02 +0100

    Create decryptor() method.

--- tasks/php/task-one/src/TextEncryption.php
+++ tasks/php/task-one/src/TextEncryption.php
@@ -5,6 +5,32 @@
  */
 class TextEncryption {
     /**
+     * decryptor() is responsible for text decryption and encryption.
+     * 
+     * @param string $textToDecrypt text to decrypt or encrypt.
+     * @param bool $isEncryptor if true, decryptor() method encrypt text instead of decrypting it.
+     * @return string
+     */
+    public function decryptor ($textToDecrypt, $isEncryptor): string {
+        $allCharConnections = $this->getCharConnections();
+        $splittedDecryptText = mb_str_split($textToDecrypt);
+
+        $charConnIndex = [0, 1]; 
+        if ($isEncryptor) $charConnIndex = [1, 0];
+
+        for ($i = 0; $i < count($splittedDecryptText); $i++) {
+            for ($j = 0; $j < count($allCharConnections); $j++) {
+                if ($splittedDecryptText[$i] === $allCharConnections[$j][$charConnIndex[0]]) {
+                    $splittedDecryptText[$i] = $allCharConnections[$j][$charConnIndex[1]];
+                    break;
+                }
+            }
+        }
+
+        return htmlspecialchars(implode($splittedDecryptText)); 
+    }
+
+    /**
      * getCharConnections() is responsible for delivery char connections.
      * 
      * @return array
