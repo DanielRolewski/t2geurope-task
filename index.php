5ca6121bc5d7e9f5ff5617dfc93ffc0b198f105e
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-08 21:45:16 +0100

    Create getCharConnections() method.

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>T2G Europe Recruitment Tasks</title>
 </head>
 <body>
     <h1>T2G Europe Recruitment Task List</h1>
+    <a href='tasks/php/task-one/main.php' alt='php/task-one'>php/task-one</a>
 </body>
 </html>
--- tasks/php/task-one/src/TextEncryption.php
+++ tasks/php/task-one/src/TextEncryption.php
@@ -4,7 +4,38 @@
  * TextEncryption is responsible for text encryption and decryption.
  */
 class TextEncryption {
-    
+    /**
+     * getCharConnections() is responsible for delivery char connections.
+     * 
+     * @return array
+     */
+    private function getCharConnections(): array {
+        $charsWithAscii = array();
+        $firstCharNum = 97;
+        $lastCharNum = 111;
+        $asciiCharOffset = 11;
+
+        //Chars connections that must be hardcoded.
+        $charsWithoutAscii = [
+            ['!', 'a'],
+            [')', 'b'],
+            ['"', 'c'],
+            ['(', 'd'],
+            ['£', 'e'],
+            ['*', 'f'],
+            ['%', 'g'],
+            ['&', 'h'],
+            ['>', 'i'],
+            ['<', 'j'],
+            ['@', 'k']
+        ];
+        
+        for ($i = $firstCharNum; $i <= $lastCharNum; $i++) {
+            array_push($charsWithAscii, [chr($i), chr($i + $asciiCharOffset)]);
+        }
+
+        return array_merge($charsWithoutAscii, $charsWithAscii);;
+    }
 }
 
 ?>
