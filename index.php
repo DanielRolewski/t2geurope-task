938e90cbe54928575e615a121ce8b6546c98bed9
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-09 21:25:37 +0100

    Add query from first SQL task to index.php

--- index.php
+++ index.php
@@ -7,5 +7,22 @@
 <body>
     <h1>T2G Europe Recruitment Task List</h1>
     <a href="tasks/php/task-one/main.php" alt="php/task-one">php/task-one</a>
+    <div>
+        <p>Pierwsze zadanie z sekcji MySQL</p>
+        <pre>
+        SELECT tickets.id AS "Winning Ticket Ids"
+        FROM `tickets`
+        LEFT JOIN `draws` ON tickets.draw_id = draws.id
+        LEFT JOIN `lotteries` ON draws.lottery_id = lotteries.id
+        WHERE lotteries.name = "GG World X"
+        AND tickets.number IN (
+            SELECT draws.won_number
+            FROM `draws`
+            LEFT JOIN `lotteries` ON lotteries.id = draws.lottery_id
+            WHERE lotteries.name = "GG World X"
+            AND draws.won_number IS NOT NULL
+        );
+        </pre>
+    </div>
 </body>
 </html>
