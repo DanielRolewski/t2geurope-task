bf4f5a9ccab1d1b4edbe1c72d9f9a6f64c2d6524
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-10 10:20:03 +0100

    Complete second SQL task.

--- index.php
+++ index.php
@@ -24,5 +24,25 @@
         );
         </pre>
     </div>
+    <div>
+        <p>Drugie zadanie z sekcji MySQL</p>
+        <pre>
+        SELECT lotteries.name,
+        SUM (lotteries.ticket_price) AS "Total Income From Tickets",
+        SUM (
+            CASE WHEN
+            tickets.bought_date < draws.draw_date
+            THEN lotteries.ticket_price
+            ELSE 0
+            END
+        ) AS "Final Profit"
+        FROM tickets
+        LEFT JOIN draws ON tickets.draw_id = draws.id
+        LEFT JOIN lotteries ON draws.lottery_id = lotteries.id
+        WHERE MONTHNAME (tickets.bought_date) = 'July'
+        AND YEAR (tickets.bought_date) = '2021'
+        GROUP BY lotteries.name;
+        </pre>
+    </div>
 </body>
 </html>
