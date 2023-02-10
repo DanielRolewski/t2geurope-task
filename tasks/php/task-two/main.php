c6d7bdffa1aa39c7fd1113033012cd3a4dd57b75
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-10 12:57:53 +0100

    Some fixes to TextEncryption.php & create main.php

---
+++ tasks/php/task-two/main.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Task Number Two</title>
+</head>
+<body>
+</body>
+</html>
