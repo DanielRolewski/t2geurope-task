cb4b44715cb970fc9e6ce4396a38e9c818a31990
DanielRolewski <daniel_rolewski@outlook.com>
2023-02-13 00:09:57 +0100

    Complete task number two form PHP section.

--- tasks/php/task-two/main.php
+++ tasks/php/task-two/main.php
@@ -5,5 +5,24 @@
     <title>Task Number Two</title>
 </head>
 <body>
+    <form name="form" action="" method="post">
+        <label>
+            Input number that will be convert to LCD:
+            <input type="text" name="numberToConvert" id="numberToConvert">
+        </label>
+        <input type="submit">
+    </form>
+    <?php
+        if (!empty($_POST['numberToConvert'])) {
+            if (is_numeric($_POST['numberToConvert'])) {
+                require_once('src/LCDDisplayNumbers.php');
+
+                $LCDDisplay = new LCDDisplayNumbers();
+                $LCDDisplay->displayLCDNumbers($_POST['numberToConvert']);
+            } else {
+                echo "<p>Entered text could only contains numbers!</p>";
+            }
+        }
+    ?>
 </body>
 </html>
